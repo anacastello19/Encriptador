f75d53513239ed821ab62dfb063f364c828d2857
Ana Castello <ana99.castello@gmail.com>
2024-02-09 09:16:49 -0300

    iniciando con javascript

---
+++ app.js
@@ -0,0 +1,21 @@
+function encriptar(){
+    let texto = document.getElementById("textoIngresar").value;
+    let tituloMensaje= document.getElementById(titulo-mensaje);
+    let parrafo = document.getElementById();
+
+    let cifrar= texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    if(texto.length !=0){
+        texto= cifrar;
+        tituloMensaje.textContent="Texto encriptado con exito";
+        parrafo.textContent="";
+        nextElementSibling.src
+
+    }
+
+}
