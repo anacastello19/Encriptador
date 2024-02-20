3b3a5d35bd56e3043936e089242eddf4251063de
Ana Castello <ana99.castello@gmail.com>
2024-02-20 13:56:26 -0300

    Encripatar y desencriptar texto listos

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 //Conectar función a su respectivo botón en el HTML
  function encriptar(){
     //Capturar el texto escrito en el campo del input del HTML
-    let encriptarText = document.getElementById("ingresarEncriptado");
+    let encriptarText = document.getElementById("ingresarDesEncriptado");
     let textEncriptado= encriptarText.value;
     console.log(textEncriptado)
+    
     let conversion= textEncriptado
         .replace(/e/gi, "enter")
         .replace(/i/gi, "imes")
@@ -15,18 +16,39 @@
 
 
     if(textEncriptado.length !=0){
+        //Se encripta el texto 
         textEncriptado=conversion;
+        //Mostramos en consola
         console.log(conversion);
+        if(document.getElementById("muñeco").innerHTML = textEncriptado){
 
+        }
     }
  }
 //Conectar función a su respectivo botón en el HTML
- function desencriptar(){
+function desencriptar(){
     //Capturar el texto escrito en el campo del input del HTML
-    const desencriptarText = document.getElementById("ingresarDesencriptado");
-    const textDesencriptado = desencriptarText.value;
-  
- }
+    let desencriptarText = document.getElementById("ingresarDesEncriptado");
+    let textDesencriptado = desencriptarText.value;
+    console.log(textDesencriptado)
+    let conversion= textDesencriptado
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if(textDesencriptado.length !=0){
+        //Se encripta el texto 
+        textDesencriptado=conversion;
+        //Mostramos en consola
+        console.log(conversion);
+        if(document.getElementById("muñeco").innerHTML = textDesencriptado){
+
+        }
+    
+    }
+}
  
 
 //Desarrollar la lógica de la encriptación
