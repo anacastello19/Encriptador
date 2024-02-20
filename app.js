c32e81c665b1f3593a39257a2f719603c835f0de
Ana Castello <ana99.castello@gmail.com>
2024-02-20 12:58:57 -0300

    Primera prueba aprobada en texto encriptar

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-function encriptar(){
-    let texto = document.getElementById("textoIngresar").value;
-    let tituloMensaje= document.getElementById(titulo-mensaje);
-    let parrafo = document.getElementById();
 
-    let cifrar= texto
+//Crear función
+//Conectar función a su respectivo botón en el HTML
+ function encriptar(){
+    //Capturar el texto escrito en el campo del input del HTML
+    let encriptarText = document.getElementById("ingresarEncriptado");
+    let textEncriptado= encriptarText.value;
+    console.log(textEncriptado)
+    let conversion= textEncriptado
         .replace(/e/gi, "enter")
         .replace(/i/gi, "imes")
         .replace(/a/gi, "ai")
-        replace(/o/gi, "ober")
+        .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
 
-    if(texto.length !=0){
+
+    if(textEncriptado.length !=0){
+        textEncriptado=conversion;
+        console.log(conversion);
+
+    }
+ }
+//Conectar función a su respectivo botón en el HTML
+ function desencriptar(){
+    //Capturar el texto escrito en el campo del input del HTML
+    const desencriptarText = document.getElementById("ingresarDesencriptado");
+    const textDesencriptado = desencriptarText.value;
+    /*console.log(textDesencriptado)
+      if(texto.length !=0){
         texto= cifrar;
         tituloMensaje.textContent="Texto encriptado con exito";
         parrafo.textContent="";
         nextElementSibling.src
 
-    }
+    }*/
+ }
+ 
 
-}
+//Desarrollar la lógica de la encriptación
+//Mostrar en la pantalla el resultado del texto encriptado
