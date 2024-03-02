69843c5f482f5c40d22d5f0cae088b8a8899a618
AniC <111471233+anacastello19@users.noreply.github.com>
2024-03-02 18:47:48 -0300

    Modificacion en JavaScript

--- app.js
+++ app.js
@@ -2,48 +2,45 @@
 //Crear función
 //Conectar función a su respectivo botón en el HTML
 function encriptar(){
-    //Boton
-    let btnCopiar= document.getElementById("copiar");
     //Capturar el texto escrito en el campo del input del HTML
-    let encriptarText = document.getElementById("ingresarDesEncriptado");
-    let textEncriptado= encriptarText.value;
-    console.log(textEncriptado)
+    const encriptarText = document.getElementById("ingresarDesEncriptado").value;
+    console.log(encriptarText)
     
-    let conversion= textEncriptado
-        .replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat");
+    const conversionLetter= {
+        e: "enter",
+        i: "imes",
+        a: "ai",
+        o: "ober",
+        u: "ufat"
+    }; 
 
+    const conversion= encriptarText.replace(/[eiaou]/gi, match => conversionMap[match]);
 
-    if(textEncriptado.length !=0){
+    if(encriptarText.length !==0){
         //Mostramos en consola
         console.log(conversion);
-        btnCopiar.style.display="block";
+        document.getElementById("copiar").style.display="block";
     }
 }
 //Conectar función a su respectivo botón en el HTML
 function desencriptar(){
     //Capturar el texto escrito en el campo del input del HTML
-    let desencriptarText = document.getElementById("ingresarDesEncriptado");
-    let textDesencriptado = desencriptarText.value;
-    console.log(textDesencriptado)
-    let conversion= textDesencriptado
-        .replace(/enter/gi, "e")
-        .replace(/imes/gi, "i")
-        .replace(/ai/gi, "a")
-        .replace(/ober/gi, "o")
-        .replace(/ufat/gi, "u");
+    const desencriptarText = document.getElementById("ingresarDesEncriptado").value;
+    console.log(desencriptarText)
 
-    if(textDesencriptado.length !=0){
-        //Se encripta el texto 
-        textDesencriptado=conversion;
-        //Mostramos en consola
+    let conversionLetter= {
+        enter: "e",
+        imes: "i",
+        ai: "a",
+        ober: "o",
+        ufat: "u"
+    };
+
+    const conversion =desencriptarText.replace(/\b(enter|imes|ai|ober|ufat)\b/gi, match => conversionMap[match]);
+
+    if(textDesencriptado.length !==0){
+        document.getElementById("muñeco").innerHTML = conversion;
         console.log(conversion);
-        if(document.getElementById("muñeco").innerHTML = textDesencriptado){
-
-        }
     
     }
 }
